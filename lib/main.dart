b8aa68840c3c1f60bec4310d0197b8b1cd8748e1
JuneSeo27 <seo83@purdue.edu>
2020-11-08 01:08:16 -0500

    pp

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+bool covid;
+
 void main() => runApp(MyApp());
 
 // class MyApp extends StatelessWidget {
@@ -26,23 +28,54 @@ class MyApp extends StatelessWidget {
       title: 'Welcome to SmartTrace',
       home: Scaffold(
           appBar: AppBar(
-            title: Text('Flutter'),
+            //alert: showAlertDialog(context),
+            title: Text('Smart Trace'),
           ),
-          body: MyLayout()),
+
+          body: Center(child: MyLayout())),
     );
   }
 }
 
 class MyLayout extends StatelessWidget {
   @override
+
   Widget build(BuildContext context) {
     return Padding(
+
       padding: const EdgeInsets.all(8.0),
       child: RaisedButton(
-        child: Text('Show alert'),
+        child: Text('Get started'),
         onPressed: () {
+
           showAlertDialog(context);
         },
+      ),
+    );
+  }
+}
+
+class yesButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Main Page"),
+      ),
+
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            if (covid == true) {
+              print("I HAVE COVID");
+            } else {
+              print("I AM COVID FREE");
+            }
+
+
+            },
+          child: Text('Go back to the start'),
+        ),
       ),
     );
   }
@@ -52,18 +85,28 @@ showAlertDialog(BuildContext context) {
   // set up the buttons
   Widget cancelButton = FlatButton(
     child: Text("Yes"),
-    onPressed: () {},
+    onPressed: () {
+      covid = true;
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => yesButton()),
+      );
+    },
   );
   Widget continueButton = FlatButton(
     child: Text("No"),
     onPressed: () {
-      Text("fuck nigg");
+      covid = false;
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => yesButton()),
+      );
     },
   );
 
   // set up the AlertDialog
   AlertDialog alert = AlertDialog(
-    title: Text("AlertDialog"),
+    title: Text("Before we continue..."),
     content:
         Text("Have you tested Positive for COVID-19 in the past 2 weeks?"),
     actions: [
